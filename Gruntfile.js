10db37666bc17b455dbe7858df62d0af240b806d
Claudia Calderas <claudia.calderas@gmail.com>
2017-04-24 13:55:34 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // uglify: {
+    //   build: {
+    //     src: 'client/scripts/*.js',
+    //     dest: 'server/public/scripts/client.min.js'
+    //   }
+    // },
+    copy: {
+      scripts: {
+        expand: true,
+        cwd: 'client/scripts',
+        src: ['client.js'],
+        dest: 'server/public/scripts/'
+      },
+      html: {
+        expand: true,
+        cwd: 'client/views',
+        src: ['index.html'],
+        dest: 'server/public/views/'
+      },
+      css: {
+        expand: true,
+        cwd: 'client/styles',
+        src: ['style.css'],
+        dest: 'server/public/styles/'
+      },
+      // bootstrap: {
+      //   expand: true,
+      //   cwd: 'node_modules/bootstrap/dist/',
+      //   src: ['css/bootstrap.css',
+      //         'js/bootstrap.js'],
+      //   dest: 'server/public/vendors/bootstrap/'
+      // },
+      angular: {
+        expand: true,
+        cwd: 'node_modules/angular/',
+        src: ['angular.js',
+              'angular.min.js',
+              'angular.min.js.map'],
+        dest: 'server/public/vendors/angular/'
+      },
+      angularmaterial: {
+        expand: true,
+        cwd: 'node_modules/angular-material/',
+        src: ['angular-material.css',
+              'angular-material.js',
+              'angular-material.min.css',
+              'angular-material.min.js'],
+        dest: 'server/public/vendors/angular-material/'
+      },
+      angularanimate: {
+        expand: true,
+        cwd: 'node_modules/angular-animate/',
+        src: ['angular-animate.js',
+              'angular-animate.min.js',
+              'angular-animate.min.js.map'],
+        dest: 'server/public/vendors/angular-animate/'
+      },
+      angulararia: {
+        expand: true,
+        cwd: 'node_modules/angular-aria/',
+        src: ['angular-aria.js',
+              'angular-aria.min.js',
+              'angular-aria.min.js.map'],
+        dest: 'server/public/vendors/angular-aria/'
+      },
+      angularmessages: {
+        expand: true,
+        cwd: 'node_modules/angular-messages/',
+        src: ['angular-messages.js',
+              'angular-messages.min.js',
+              'angular-messages.min.js.map'],
+        dest: 'server/public/vendors/angular-messages/'
+      }
+    },
+    watch: {
+      files: [
+        'client/**/*.*'
+      ],
+      tasks: ['copy']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['copy', 'watch']);
+};
