1af910df176a3afba851deb6362c3d753dbb5b0e
Claudia Calderas <claudia.calderas@gmail.com>
2017-04-24 15:15:32 -0500

    passport authentication added to project

--- server/app.js
+++ server/app.js
@@ -1,29 +1,72 @@
-//BASE MODULES
 var express = require('express');
 var app = express();
-var path = require('path');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var path = require('path');
 
-//DATABASE MODULE
-var db = require('./modules/db.js');
+var passport = require('./strategies/userStrategy');
+var session = require('express-session');
 
-//ROUTE MODULES
-var index = require('./routes/index.js');
+// Route includes
+var index = require('./routes/index');
+var user = require('./routes/user');
+var register = require('./routes/register');
 
-//APP CONFIG
-app.set('port', (process.env.PORT || 5000));
-
-//MIDDLEWARE CONFIG
+// Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('server/public'));
 
-//ROUTES
+// Serve back static files
+app.use(express.static('./server/public'));
+
+// Passport Session Configuration //
+app.use(session({
+   secret: 'secret',
+   key: 'user', // this is the name of the req.variable. 'user' is convention, but not required
+   resave: 'true',
+   saveUninitialized: false,
+   cookie: { maxage: 60000, secure: false }
+}));
+
+// start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/register', register);
+app.use('/user', user);
 app.use('/', index);
 
-//LISTEN
-app.listen(app.get('port'), function(){
-    console.log('Listening on port: ', app.get('port'));
+// Mongo Connection //
+var mongoURI = '';
+// process.env.MONGODB_URI will only be defined if you
+// are running on Heroku
+if(process.env.MONGODB_URI != undefined) {
+    // use the string value of the environment variable
+    mongoURI = process.env.MONGODB_URI;
+} else {
+    // use the local database server
+    mongoURI = 'mongodb://localhost:27017/passport';
+}
+
+// var mongoURI = "mongodb://localhost:27017/passport";
+var mongoDB = mongoose.connect(mongoURI).connection;
+
+mongoDB.on('error', function(err){
+   if(err) {
+     console.log("MONGO ERROR: ", err);
+   }
+   res.sendStatus(500);
 });
 
-module.exports = app;
+mongoDB.once('open', function(){
+   console.log("Connected to Mongo, meow!");
+});
+
+// App Set //
+app.set('port', (process.env.PORT || 7070));
+
+// Listen //
+app.listen(app.get("port"), function(){
+   console.log("Listening on port: " + app.get("port"));
+});
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,119 @@
-console.log('client.js');
+var myApp = angular.module('myApp', ['ngRoute']);
+
+/// Routes ///
+myApp.config(['$routeProvider', '$locationProvider',
+      function($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix('');
+
+  $routeProvider
+    .when('/home', {
+      templateUrl: '/views/templates/home.html',
+      controller: 'LoginController',
+    })
+    .when('/register', {
+      templateUrl: '/views/templates/register.html',
+      controller: 'LoginController'
+    })
+    .when('/user', {
+      templateUrl: '/views/templates/user.html',
+      controller: 'UserController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+    .when('/info', {
+      templateUrl: '/views/templates/info.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+    .otherwise({
+      redirectTo: 'home'
+    });
+}]);
+
+myApp.controller('InfoController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
+  $scope.logout = UserService.logout;
+}]);
+
+myApp.controller('LoginController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
+    $scope.user = {
+      username: '',
+      password: ''
+    };
+    $scope.message = '';
+
+    $scope.login = function() {
+      if($scope.user.username === '' || $scope.user.password === '') {
+        $scope.message = "Enter your username and password!";
+      } else {
+        console.log('sending to server...', $scope.user);
+        $http.post('/', $scope.user).then(function(response) {
+          if(response.data.username) {
+            console.log('success: ', response.data);
+            // location works with SPA (ng-route)
+            $location.path('/user');
+          } else {
+            console.log('failure: ', response);
+            $scope.message = "Wrong!!";
+          }
+        });
+      }
+    };
+
+    $scope.registerUser = function() {
+      if($scope.user.username === '' || $scope.user.password === '') {
+        $scope.message = "Choose a username and password!";
+      } else {
+        console.log('sending to server...', $scope.user);
+        $http.post('/register', $scope.user).then(function(response) {
+          console.log('success');
+          $location.path('/home');
+        },
+        function(response) {
+          console.log('error');
+          $scope.message = "Please try again."
+        });
+      }
+    }
+}]);
+
+myApp.controller('UserController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
+  $scope.userObject = UserService.userObject;
+  $scope.logout = UserService.logout;
+}]);
+
+myApp.factory('UserService', ['$http', '$location', function($http, $location){
+  console.log('User Service Loaded');
+
+  var userObject = {};
+
+  return {
+    userObject : userObject,
+
+    getuser : function(){
+      $http.get('/user').then(function(response) {
+          if(response.data.username) {
+              // user has a curret session on the server
+              userObject.userName = response.data.username;
+              console.log('User Data: ', userObject.userName);
+          } else {
+              // user has no session, bounce them back to the login page
+              $location.path("/home");
+          }
+      });
+    },
+
+    logout : function() {
+        $http.get('/user/logout').then(function(response) {
+          console.log('logged out');
+          $location.path("/home");
+        });
+    }
+  };
+}]);
