17350d64ed08deb5ffcd3cde7b9d63136cd37b7a
Claudia Calderas <claudia.calderas@gmail.com>
2017-05-04 12:00:09 -0500

    moved database connection to db.js and pointed db to mLab

--- server/app.js
+++ server/app.js
@@ -16,6 +16,8 @@ var register = require('./routes/register');
 var recipe = require('./routes/recipe');
 var group = require('./routes/group');
 
+var mongoDB = require('./modules/db');
+
 // Body parser middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -44,32 +46,6 @@ app.use('/recipe', recipe);
 app.use('/group', group);
 app.use('/uploads', uploads);
 app.use('/', index);
-
-// Mongo Connection //
-var mongoURI = '';
-// process.env.MONGODB_URI will only be defined if you
-// are running on Heroku
-if(process.env.MONGODB_URI != undefined) {
-    // use the string value of the environment variable
-    mongoURI = process.env.MONGODB_URI;
-} else {
-    // use the local database server
-    mongoURI = 'mongodb://localhost:27017/heritagecuisine';
-}
-
-// var mongoURI = "mongodb://localhost:27017/passport";
-var mongoDB = mongoose.connect(mongoURI).connection;
-
-mongoDB.on('error', function(err){
-   if(err) {
-     console.log("MONGO ERROR: ", err);
-   }
-   res.sendStatus(500);
-});
-
-mongoDB.once('open', function(){
-   console.log("Connected to Mongo, meow!");
-});
 
 // App Set //
 app.set('port', (process.env.PORT || 7070));
