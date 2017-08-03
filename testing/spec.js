7125bb1972c2088d7efe5d159e15f9f8f2df000c
Claudia Calderas <claudia.calderas@gmail.com>
2017-08-02 20:27:40 -0500

    added test scenarios

--- testing/spec.js
+++ testing/spec.js
@@ -1,5 +1,5 @@
 // spec.js
-describe('heritagecuisine', function() {
+describe('heritagecuisine correct login', function() {
   it('should let user login the app', function() {
     browser.get('http://localhost:7070/#/home');
     element(by.model('user.username')).sendKeys('claudia');
@@ -9,5 +9,21 @@ describe('heritagecuisine', function() {
 
     var newUrl = browser.getCurrentUrl();
     expect(newUrl).toEqual('http://localhost:7070/#/user');
+  });
+});
+
+describe('heritagecuisine incorrect login', function() {
+  it('should not let user login the app with incorrect credentials', function() {
+    browser.get('http://localhost:7070/#/home');
+    element(by.model('user.username')).sendKeys('claudia');
+    element(by.model('user.password')).sendKeys('dog');
+
+    element(by.id('loginSubmit')).click();
+
+    var errorMsg = element(by.id('loginMessage')).getText();
+    expect(errorMsg).toEqual('Invalid combination of username and password.');
+
+    var currentUrl = browser.getCurrentUrl();
+    expect(currentUrl).toEqual('http://localhost:7070/#/home');
   });
 });
